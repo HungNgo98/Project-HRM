311b9459d9691fff5a545a5d6b98a5279e5d32d3
hoangvd <vudinhhoang358@gmailo.com>
2021-01-18 17:42:46 +0700

    update

--- Project-HRM/routes/api.php
+++ Project-HRM/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\EmployessAPIController;
+use App\Http\Controllers\API\DepartmentsAPIController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +23,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::resource('positions', App\Http\Controllers\API\PositionsAPIController::class);
 
-Route::resource('departments', App\Http\Controllers\API\DepartmentsAPIController::class);
+Route::resource('departments', DepartmentsAPIController::class);
 
-Route::resource('employesses', App\Http\Controllers\API\EmployessAPIController::class);
+Route::resource('employesses', EmployessAPIController::class);
